98a6ef84b007b357be39d9805669f6026af9a5a8
minimo-ai <maureenmcappallo@gmail.com>
2026-01-16 21:32:38 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/og-image.png',
+        headers: [
+          { key: 'Content-Type', value: 'image/png' },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
